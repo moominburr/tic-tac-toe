3417b56ad4f593c5f77ec7c620e293c0eb44c431
Katherine Burr <katherinevb@hotmail.co.uk>
2021-08-23 15:46:40 +0100

    Winning conditions checked, winning animation added

--- js/script.js
+++ js/script.js
@@ -30,39 +30,127 @@ const Player = (name, sign) => {
 
 const playerX = Player("Kate", "X");
 
-const squares = document.querySelectorAll(".gamesquare");
-const computerTurn = function () {
-  const turn = function () {
+const squares = document.querySelectorAll(".square-text");
+
+const initializeGame = (() => {
+  let winningArray = [];
+  let winner = null;
+  squares.forEach((square) =>
+    square.addEventListener("click", (e) => {
+      playRound(e, playerX);
+    })
+  );
+  const compTurn = () => {
     let num = Math.floor(Math.random() * 9);
-    console.log(num);
+    let array = Gameboard.getGamearray();
+    while (array[num] !== "") {
+      num = Math.floor(Math.random() * 9);
+    }
+    return num;
   };
-  return turn;
-};
-
-const initializeGame = (e, player) => {
-  let index = e.target.id;
-  let array = Gameboard.getGamearray();
-  let sign = player.getSign();
-  let playingRound = false;
-  if (array[index] !== "") {
-    alert("Oops");
-    return;
-  } else {
-    Gameboard.setGameArray(index, sign);
-    e.target.textContent = sign;
-    console.log(Gameboard.getGamearray());
-    playingRound = true;
-  }
-  return {playingRound}
-};
-
-squares.forEach((square) =>
-  square.addEventListener("click", (e) => {
-      if(initializeGame.playingRound) {
-        
-      } else {
-        initializeGame(e, playerX);
+  const checkWinningConditions = () => {
+    let array = Gameboard.getGamearray();
+    let i = 0;
+    array.forEach((el) => {
+      if (el === "") i++;
+    });
+    if (i === 8) return;
+    //return true if there is a winner
+    //update winner's name
+    const checkDiagonals = () => {
+      let d1 = false;
+      let d2 = false;
+      if (array[0] === array[4] && array[8] === array[0]) {
+        array[0] === "" ? (d1 = false) : (d1 = true && winningArray.push("0", "4", "8"));
       }
-    
-  })
-);
+      if (array[2] === array[4] && array[6] === array[2]) {
+        array[2] === "" ? (d2 = false) : (d2 = true && winningArray.push("2", "4", "6"));
+      }
+      if (d1 || d2) {
+        if (d1) winner = array[0];
+        if (d2) winner = array[2];
+        return true;
+      } else {
+        return false;
+      }
+    };
+    const checkVerticals = () => {
+      let v1 = false;
+      let v2 = false;
+      let v3 = false;
+      if (array[0] === array[3] && array[6] === array[0]) {
+        array[0] === "" ? (v1 = false) : (v1 = true && winningArray.push("0", "3", "6"));
+      }
+      if (array[1] === array[4] && array[7] === array[1]) {
+        array[1] === "" ? (v2 = false) : (v2 = true && winningArray.push("1", "4", "7"));
+      }
+      if (array[2] === array[5] && array[8] === array[2]) {
+        array[2] === "" ? (v3 = false) : (v3 = true && winningArray.push("2", "5", "8"));
+      }
+      if (v1 || v2 || v3) {
+        if (v1) winner = array[0];
+        if (v2) winner = array[1];
+        if (v3) winner = array[2];
+        return true;
+      } else {
+        return false;
+      }
+    };
+    const checkHorizontals = () => {
+      let h1 = false;
+      let h2 = false;
+      let h3 = false;
+      if (array[0] === array[1] && array[2] === array[0]) {
+        array[0] === "" ? (h1 = false) : (h1 = true && winningArray.push("0", "1", "2"));
+      }
+      if (array[3] === array[4] && array[5] === array[3]) {
+        array[3] === "" ? (h2 = false) : (h2 = true && winningArray.push("3", "4", "5"));
+      }
+      if (array[6] === array[7] && array[8] === array[6]) {
+        array[6] === "" ? (h3 = false) : (h3 = true && winningArray.push("6", "7", "8"));
+      }
+      if (h1 || h2 || h3) {
+        if (h1) winner = array[0];
+        if (h2) winner = array[3];
+        if (h3) winner = array[6];
+        return true;
+      } else {
+        return false;
+      }
+    };
+    //const checkDraw
+    let checkD = checkDiagonals();
+    let checkV = checkVerticals();
+    let checkH = checkHorizontals();
+
+    if (checkD || checkV || checkH) {
+      console.log(`${checkD}, ${checkV}, ${checkH}`);
+      return true;
+    }
+  };
+  const playRound = (e, player) => {
+    let isWinner = false;
+    while (!isWinner) {
+      let index = e.target.id;
+      let array = Gameboard.getGamearray();
+      let sign = player.getSign();
+      if (array[index] !== "") {
+        console.log(index);
+        return;
+      } else {
+        Gameboard.setGameArray(index, sign);
+        e.target.textContent = sign;
+      }
+      let ct = compTurn();
+      Gameboard.setGameArray(ct, "O");
+      squares[ct].textContent = "O";
+      console.log(Gameboard.getGamearray());
+      isWinner = checkWinningConditions();
+    }
+    console.log(isWinner);
+    console.log(winner);
+    winningArray.forEach((el) => {
+      squares[el].classList.add("winner");
+    });
+  };
+})();
