aaf5d3f551c772f3c7a1d8cce1c6be09e3cd7aa9
Katherine Burr <katherinevb@hotmail.co.uk>
2021-08-23 18:12:00 +0100

    Conditions check now working in each part of the game

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ const initializeGame = (() => {
     }
     return num;
   };
-  const checkWinningConditions = () => {
+  const checkWinningConditions = (() => {
     let drawCheck = [];
     let array = Gameboard.getGamearray();
     let i = 0;
@@ -57,17 +57,18 @@ const initializeGame = (() => {
       if (el === "") i++;
     });
     if (i === 8) return;
-    //return true if there is a winner
-    //update winner's name
-
     const checkDiagonals = () => {
       let d1 = false;
       let d2 = false;
       if (array[0] === array[4] && array[8] === array[0]) {
-        array[0] === "" ? (d1 = false) : (d1 = true && winningArray.push("0", "4", "8") && drawCheck.push(array[0]));
+        array[0] === ""
+          ? (d1 = false)
+          : (d1 = true && winningArray.push("0", "4", "8") && drawCheck.push(array[0]));
       }
       if (array[2] === array[4] && array[6] === array[2]) {
-        array[2] === "" ? (d2 = false) : (d2 = true && winningArray.push("2", "4", "6") && drawCheck.push(array[2]));
+        array[2] === ""
+          ? (d2 = false)
+          : (d2 = true && winningArray.push("2", "4", "6") && drawCheck.push(array[2]));
       }
       if (d1 || d2) {
         if (d1) winner = array[0];
@@ -76,19 +77,24 @@ const initializeGame = (() => {
       } else {
         return false;
       }
+      
     };
     const checkVerticals = () => {
       let v1 = false;
       let v2 = false;
       let v3 = false;
       if (array[0] === array[3] && array[6] === array[0]) {
-        array[0] === "" ? (v1 = false) : (v1 = true && winningArray.push("0", "3", "6") && drawCheck.push(array[0]));
-      }
-      if (array[1] === array[4] && array[7] === array[1]) {
-        array[1] === "" ? (v2 = false) : (v2 = true && winningArray.push("1", "4", "7") && drawCheck.push(array[1]));
-      }
-      if (array[2] === array[5] && array[8] === array[2]) {
-        array[2] === "" ? (v3 = false) : (v3 = true && winningArray.push("2", "5", "8") && drawCheck.push(array[2]));
+        array[0] === ""
+          ? (v1 = false)
+          : (v1 = true && winningArray.push("0", "3", "6") && drawCheck.push(array[0]));
+      } else if (array[1] === array[4] && array[7] === array[1]) {
+        array[1] === ""
+          ? (v2 = false)
+          : (v2 = true && winningArray.push("1", "4", "7") && drawCheck.push(array[1]));
+      } else if (array[2] === array[5] && array[8] === array[2]) {
+        array[2] === ""
+          ? (v3 = false)
+          : (v3 = true && winningArray.push("2", "5", "8") && drawCheck.push(array[2]));
       }
       if (v1 || v2 || v3) {
         if (v1) winner = array[0];
@@ -104,13 +110,19 @@ const initializeGame = (() => {
       let h2 = false;
       let h3 = false;
       if (array[0] === array[1] && array[2] === array[0]) {
-        array[0] === "" ? (h1 = false) : (h1 = true && winningArray.push("0", "1", "2") && drawCheck.push(array[0]));
+        array[0] === ""
+          ? (h1 = false)
+          : (h1 = true && winningArray.push("0", "1", "2") && drawCheck.push(array[0]));
       }
       if (array[3] === array[4] && array[5] === array[3]) {
-        array[3] === "" ? (h2 = false) : (h2 = true && winningArray.push("3", "4", "5") && drawCheck.push(array[3]));
+        array[3] === ""
+          ? (h2 = false)
+          : (h2 = true && winningArray.push("3", "4", "5") && drawCheck.push(array[3]));
       }
       if (array[6] === array[7] && array[8] === array[6]) {
-        array[6] === "" ? (h3 = false) : (h3 = true && winningArray.push("6", "7", "8") && drawCheck.push(array[6]));
+        array[6] === ""
+          ? (h3 = false)
+          : (h3 = true && winningArray.push("6", "7", "8") && drawCheck.push(array[6]));
       }
       if (h1 || h2 || h3) {
         if (h1) winner = array[0];
@@ -121,44 +133,48 @@ const initializeGame = (() => {
         return false;
       }
     };
-    //const checkDraw
-    let checkD = checkDiagonals();
-    let checkV = checkVerticals();
-    let checkH = checkHorizontals();
-    for(i = 0; i < drawCheck.length; i++){
-        if(i === 0) return
-        if(drawCheck[i] === drawCheck[i-1]) return;
-        else isDraw = true;
-    }
-    if (checkD || checkV || checkH) {
-      console.log(`${checkD}, ${checkV}, ${checkH}`);
-      return true;
-    }
-  };
+
+    const overall = () => {
+      let ch = checkHorizontals();
+      let cv = checkVerticals();
+      let cd = checkDiagonals();
+      if (cd || cv || ch) {
+          console.log('TRUE')
+        return true;
+      }
+    };
+    return { overall };
+  })();
+  const endGame = () => {
+      const popUp = document.querySelector('.end-game-pop-up');
+      popUp.classList.add('pop-up-active');
+      
+  }
   const playRound = (e, player) => {
     let isWinner = false;
+    let iteration = 1;
     while (!isWinner) {
-      let index = e.target.id;
-      let array = Gameboard.getGamearray();
-      let sign = player.getSign();
-      if (array[index] !== "") {
-        console.log(index);
-        return;
+      if (iteration % 2 !== 0) {
+        let index = e.target.id;
+        let array = Gameboard.getGamearray();
+        let sign = player.getSign();
+        if (array[index] !== "") {
+          return;
+        } else {
+          Gameboard.setGameArray(index, sign);
+          e.target.textContent = sign;
+        }
+        iteration++;
+        let midRoundWinner = checkWinningConditions.overall();
+        console.log(midRoundWinner);
+        if (midRoundWinner) endGame();
       } else {
-        Gameboard.setGameArray(index, sign);
-        e.target.textContent = sign;
+        let ct = compTurn();
+        Gameboard.setGameArray(ct, "O");
+        squares[ct].textContent = "O";
+        iteration++;
+        isWinner = checkWinningConditions.overall();
       }
-      let ct = compTurn();
-      Gameboard.setGameArray(ct, "O");
-      squares[ct].textContent = "O";
-      console.log(Gameboard.getGamearray());
-      isWinner = checkWinningConditions();
     }
-
-    console.log(isWinner);
-    console.log(winner);
-    winningArray.forEach((el) => {
-      squares[el].classList.add("winner");
-    });
   };
 })();
